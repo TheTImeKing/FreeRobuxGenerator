c82c7444050c5a1751c1db506ad0f0c87c86c6c2
TheTImeKing <114049849+TheTImeKing@users.noreply.github.com>
2023-05-10 09:41:31 +0300

    script.js

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const coolButton = document.querySelector('.js-cool');
 coolButton.addEventListener("click", () => {
     window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley');
 });
+coolButton.addEventListener('mouseover', () => {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley')
+})
